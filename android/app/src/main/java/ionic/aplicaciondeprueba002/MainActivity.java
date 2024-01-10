1d49594312e8c2081dc7391555f57d91f8072dbd
Tomas-Leiva-Duran <to.leiva@duocuc.cl>
2024-01-10 19:57:30 -0300

    ok

---
+++ android/app/src/main/java/ionic/aplicaciondeprueba002/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.aplicaciondeprueba002;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'calculadora',
+    loadComponent: () => import('./calculadora/calculadora.page').then( m => m.CalculadoraPage)
+  },
 ];
